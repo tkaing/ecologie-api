bf9585d0d46cc90bda24c60ae0dc2b6caa509eda
tkaing <t.kaing13@gmail.com>
2019-04-25 21:50:58 +0200

    Ajout des variables pour la connexion à la base de données

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+var MONGODB_DBNAME = 'ecologie-api';
+var MONGODB_COLLEC = 'users';
+
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
 var express = require('express');
 var router = express.Router();
 
